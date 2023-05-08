700076e4bf01523c4a1a4dc18febda4dd880f257
Samia Shorna <samiasharnami225@gmail.com>
2023-05-09 00:38:40 +0600

    second commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// let height = screen.height;
+// function vh(percent) {
+//     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+//     return (percent * h) / 100;
+//   }
+
+//   console.log(vh(90))
+//   console.log(window.innerHeight)
+// // if (screen.height > vh(90)){
+// //     document.getElementById("home").style.color = "black";
+// // }
+// // else {
+// //     document.getElementById("home").style.color = rgb(232, 223, 225);
+// // }
