8d85e508cd797c7e2f3e97b75c9476e034b2230b
Samia Shorna <samiasharnami225@gmail.com>
2023-06-23 21:21:43 +0600

    eighth commit

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ include "includes/connect.php";
     <title>Samia Sharnami</title>
     <link rel="icon" href="img/dp.jpg" type="image/gif/png">
     <!-- Font Awesome -->
-    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" type='text/css'/> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Raleway font -->
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
@@ -103,20 +102,6 @@ include "includes/connect.php";
         <!-- Navbar -->
         <!-- </div> -->
 
-        
-        
-        <!-- <div class="text text-end text-dark d-flex justify-content-center" style="padding-top: 200px;" data-aos="fade-up">
-            <figure class="p-3">
-                <blockquote class="blockquote pt-3" style="padding-right: 0px;">
-                    <p class="pb-2 font-italic">
-                      <em style="color: black;">"The future depends on what you do today."</em>                      
-                    </p>
-                    </blockquote>
-                    <figcaption class="blockquote-footer mb-0 pb-3 align-content-end" style="color: black;">
-                        Mahatma Gandhi
-                    </figcaption>
-            </figure>
-        </div> -->
 
         <div class="me" data-aos="fade-up-left">
             <img src="img/IMG_20201115_225732_887.jpg" alt="shorna" class="me-pic">
@@ -169,9 +154,10 @@ include "includes/connect.php";
                 <div>                    
                     <div class="container text-center text-sm-start my-4">
                         <h1>Samia Sharnami</h1>
-                        <p>I am a Front-end developer. I'm enthusiastic about web development and problem solving. I have a deep understanding of web development 
-                            technologies, including HTML, CSS, PHP, and JavaScript, and I use my skills to create responsive and interactive websites and applications 
-                            that work seamlessly across different devices and platforms.</p>
+                        <p>I aspire to be a Full-stack software engineer. I'm enthusiastic about web development and problem solving. 
+                            I have a deep understanding of web development technologies, including HTML, CSS, PHP, and JavaScript, 
+                            and I want to use my skills to create responsive and interactive websites and applications that work 
+                            seamlessly across different devices and platforms.</p>
                     </div>                    
                 </div>
             </div>
@@ -198,11 +184,39 @@ include "includes/connect.php";
                     <i class="fa-solid fa-calendar" style="color: #36507d;"></i>
                     <span class="ms-2">January 2018 - January 2023</span> 
                 </div>
-                
             </div>
         </div>
-
+        <div class="container mt-3 hover-shadow">
+            <div class="wrapper d-sm-flex">
+                <img src="img/kgc.png" class="mt-5 ms-2 mb-2 align-content-center" alt="uni" style="height: 150px; width: 100px;">
+                <div class="p-sm-5 justify-content">
+                    <p class="fs-4 fw-bold">Higher Secondary Certificate</p>
+                    <p class="fw-bold">
+                        GPA 4.67 out of 5.00 <br>
+                        Kushtia Government College
+                    </p>
+                    <i class="fa-solid fa-calendar" style="color: #36507d;"></i>
+                    <span class="ms-2">July 2015 - March 2017</span> 
+                </div>
+            </div>
+        </div>
+        <div class="container mt-3 hover-shadow">
+            <div class="wrapper d-sm-flex">
+                <img src="img/bgpsc.png" class="mt-5 ms-2 mb-2 align-content-center" alt="uni" style="height: 150px; width: 100px;">
+                <div class="p-sm-5 justify-content">
+                    <p class="fs-4 fw-bold">Secondary School Certificate</p>
+                    <p class="fw-bold">
+                        GPA 5.00 out of 5.00 <br>
+                        Border Guard Public School and College <br>
+                        Mirpur, Kushtia
+                    </p>
+                    <i class="fa-solid fa-calendar" style="color: #36507d;"></i>
+                    <span class="ms-2">January 2013 - January 2015</span> 
+                </div>
+            </div>
+        </div>
     </section>
+
 
     <!-- Skills -->
     <section class="m-5" id="skills" data-aos="fade-up">
@@ -231,8 +245,7 @@ include "includes/connect.php";
                         <h2><?php echo $fetch['skill']; ?></h2>
                     </div>
                 </div>
-                <?php $count = $count + 1; }?>   
-                
+                <?php $count = $count + 1; }?>                
             </div>
 
             <div class="d-flex justify-content-center mt-5">
@@ -240,44 +253,88 @@ include "includes/connect.php";
                 <i class="fa-solid fa-cloud-arrow-down"></i> Download CV
                 </a>
             </div>
-
         </div>
     </section>
     
     
     <!-- Projects -->
-    <section class="m-5" id="projects" data-aos="fade-up">
+    <section class="m-5 overflo-auto" id="projects" data-aos="fade-up">
         <div class="container">
             <h1>Projects</h1>
             <hr style="width: 98%;">
         </div>
 
-        <div class="container d-flex justify-content-center">
-            <div class="row">
+        <div class="alert alert-warning alert-dismissible fade show container" role="alert">
+            <p> <strong>Click</strong> on the images to show preview.</p>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+
+        <div class="container">
+            <div class="row d-flex justify-content-center">
                 <div class="col-lg-6" data-aos="zoom-in">
-                    <div class="image mt-5 mx-sm-5 mb-5">
+                    <div class="image mt-5 mx-sm-5 mb-3">
                         <img src="img/library.png" alt="library" class="image__img">
                         <div class="image__overlay image__overlay__blur">
-                            <a href="#" target="_blank" class="image__desc">Show preview  <i class="fa-solid fa-arrow-right" style="color: white;"></i></a>
-                        </div>
-                        <h4 class="mt-4 mb-5 text-center" style="padding-bottom: 500px;">A Laravel based Library Management System</h4>
+                            <a href="https://cseseminarlibrary.000webhostapp.com/" target="_blank" class="image__desc">Show preview  <i class="fa-solid fa-arrow-right" style="color: white;"></i></a>
+                        </div>                        
                     </div>
+                    <div class="bottom-text mx-sm-5 mt-5">
+                        <h4 class="text-center">CSE Seminar Library</h4>
+                        <p class="text-justify" style="color: gray;">CSE Seminar Library is a Library Management System which can be used to search, view, and issue books, check which book is issued
+                            and by whom. Get code from <a href="https://github.com/shorna33/library.git" target="_blank">here</a>.</p>
+                        <p class="text-justify" style="color: gray;">Language and Technologies used: Laravel, Bootstrap, PHP, MySQL.</p>
+                    </div>                    
                 </div>
-                <div class="col-lg-6 mt-5 mt-sm-0" data-aos="zoom-in">
-                    <div class="image mt-5 mx-sm-5 mb-5">
+                <div class="col-lg-6 mt-5 mt-sm-0 second" data-aos="zoom-in">
+                    <div class="image mt-5 mx-sm-5 mb-3">
                         <img src="img/tourism.png" alt="library" class="image__img">
                         <div class="image__overlay image__overlay__blur">
                             <a href="http://tourville.great-site.net/" target="_blank" class="image__desc">Show preview  <i class="fa-solid fa-arrow-right" style="color: white;"></i></a>
-                            
-                        </div>
-                        <h4 class="mt-4 mb-5 text-center">A PHP and MySQL based Tourism Management System</h4>
+                        </div>                        
                     </div>
+                    <div class="bottom-text mx-sm-5 mt-5">
+                        <h4 class="text-center">Tourville</h4>
+                        <p class="text-justify" style="color: gray;">Tourville is Tourism Management System which can be used to view and search tour agencies and packages. Packages can also
+                            be booked from this application. Get code from <a href="https://github.com/shorna33/tourville.git" target="_blank">here</a>.</p>
+                        <p class="text-justify" style="color: gray;">Language and Technologies used: PHP, Bootstrap, MySQL.</p>
+                    </div>                    
                 </div>
             </div>
+
+            <div class="row d-flex justify-content-center second-row">
+                <div class="col-lg-6 second" data-aos="zoom-in">
+                    <div class="image mt-5 mx-sm-5 mb-3">
+                        <img src="img/crud.png" alt="blog" class="image__img">
+                        <div class="image__overlay image__overlay__blur">
+                            <a href="http://personalblogposts.000webhostapp.com/" target="_blank" class="image__desc">Show preview  <i class="fa-solid fa-arrow-right" style="color: white;"></i></a>
+                        </div>                        
+                    </div>
+                    <div class="bottom-text mx-sm-5 mt-5">
+                        <h4 class="text-center">Personal Blog Application</h4>
+                        <p class="text-justify" style="color: gray;">A personal blog post application which can be used to post, edit, and delete simple blogs. Get code from 
+                        <a href="https://github.com/shorna33/Laravel-CRUD-Application.git" target="_blank">here</a>.</p>
+                        <p class="text-justify" style="color: gray;">Language and Technologies used: Laravel, Bootstrap, PHP, MySQL.</p>
+                    </div>                    
+                </div>
+                <div class="col-lg-6 mt-5 mt-sm-0 second" data-aos="zoom-in">
+                    <div class="image mt-5 mx-sm-5 mb-3">
+                        <img src="img/portfolio.png" alt="portfolio" class="image__img">
+                        <div class="image__overlay image__overlay__blur">
+                            <a href="#" target="_blank" class="image__desc">Show preview  <i class="fa-solid fa-arrow-right" style="color: white;"></i></a>
+                        </div>                        
+                    </div>
+                    <div class="bottom-text mx-sm-5 mt-5">
+                        <h4 class="text-center">Portfolio</h4>
+                        <p class="text-justify" style="color: gray;">Get code from <a href="https://github.com/shorna33/Portfolio.git" target="_blank">here</a>.</p>
+                        <p class="text-justify" style="color: gray;">Language and Technologies used: PHP, Bootstrap, JavaScript, MySQL.</p>
+                    </div>                    
+                </div>
+            </div>
+            
         </div>
         
 
-        <div style="height: 100px;"></div>
+        <!-- <div style="height: 150px;"></div> -->
     </section>
 
 
@@ -430,7 +487,7 @@ include "includes/connect.php";
                 text.textContent = 'Programmer';
             }, 0);
             setTimeout(() => {
-                text.textContent = 'Front-end Developer';
+                text.textContent = 'Full-stack Developer';
             }, 4000);
         }
 
